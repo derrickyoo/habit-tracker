d9999de7c9328524b7bed48ccf646ed7f24edf91
Derrick Yoo <derrickyoo@gmail.com>
2026-04-30 11:10:55 -0700

    demo cake route

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,19 @@
 import express from "express";
+import { kMaxLength } from "node:buffer";
 
 const app = express();
 
 app.get("/health", (_, res) => {
 	res.json({ message: "hello" }).status(200);
+});
+
+app.post("/cake", (_, res) => {
+	res.send("ok");
+});
+
+app.get("/cake/:name/:id", (req, res) => {
+	console.log(req.params.name, req.params.id);
+	res.send({ name: req.params.name, id: req.params.id });
 });
 
 export { app };
