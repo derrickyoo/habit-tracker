8e6dd38f3fef8d00b16d417cbbf6d403d6134251
Derrick Yoo <derrickyoo@gmail.com>
2026-05-01 10:10:48 -0700

    update register route handler

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,12 +1,11 @@
 import { Router } from "express";
+import { register } from "../controllers/authController.ts";
 import { userInsertSchema } from "../db/schema.ts";
 import { validateBody } from "../middleware/validation.ts";
 
 const router = Router();
 
-router.post("/register", validateBody(userInsertSchema), (req, res) => {
-	res.status(201).json({ message: "user sign up" });
-});
+router.post("/register", validateBody(userInsertSchema), register);
 
 router.post("/login", (req, res) => {
 	res.status(201).json({ message: "user logged in" });
